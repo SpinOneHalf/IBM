1ecb65cb7e9e39093505481363a7c20f097bbb1b
charlestheurkauf <charlestheurkauf@outlook.com>
2023-02-25 08:03:02 -0500

    First Light and we are already working!

---
+++ displayresults/2dgif.py
@@ -0,0 +1,48 @@
+from utils import simulation, RiamanProblemInit
+import matplotlib.pyplot as plt
+import glob
+import contextlib
+from PIL import Image
+import numpy as np
+import os
+
+
+tf = .03
+tlocal = 0.
+nx = 5750
+ny = 5750
+r, u, v, p, E, c, dx, dy = RiamanProblemInit(nx, ny)
+
+dtstep = .001
+stepint = 0
+while tlocal <= tf:
+    tlocal += dtstep
+    (r, u, v, p, E, c, tlocal) = simulation(r, u, v, E, p, c,
+                                            dx, dy, ny, nx, tlocal)
+    print(f'local {tlocal}')
+    fig = plt.figure(1)
+    plt.imshow(r)
+    plt.savefig(f"temp/roe_step_{stepint}.png")
+    stepint += 1
+# filepaths
+fp_in = "temp/roe_step_*.png"
+fp_out = "image_roe.gif"
+
+# use exit stack to automatically close opened images
+with contextlib.ExitStack() as stack:
+    stuff = glob.glob(fp_in)
+    idxs = []
+    for pic in stuff:
+        idx = int(pic.split('step_')[-1].split('.')[0])
+        idxs.append(idx)
+    # lazily load images
+    images=np.array(stuff)[np.argsort(idxs)]
+    imgs = (stack.enter_context(Image.open(f))
+            for f in images)
+
+    # extract  first image from iterator
+    img = next(imgs)
+
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+    img.save(fp=fp_out, format='GIF', append_images=imgs,
+             save_all=True, duration=200, loop=0)
--- utils.py
+++ utils.py
@@ -1,21 +1,53 @@
+import matplotlib.pyplot as plt
 import numpy as np
-
+from numba import njit,prange
 # Parameters
 gamma = 1.4
 CFL = 0.5
 
 
-def simulation(u0):
-    return u0
 
+@njit(parallel=True)
+def update(Qnew, dt, dx, dy, nx, ny, Fh, Gh):
+    for j in prange(1, ny - 1):
+        for i in prange(1, nx - 1):
+            Qnew[i, j, :] = Qnew[i, j, :] - (dt / dx) * (Fh[i, j, :] - Fh[i - 1, j, :]) - (dt / dy) * (
+                    Gh[i, j, :] - Gh[i, j - 1, :])
+    return Qnew
 
+@njit
+def simulation(r, u, v, E, p,c,
+               dx, dy, ny, nx, tMax
+               ):
+    t = 0
+    while t < tMax:
+        dt = CFL * min(dx, dy) / np.max(c + np.sqrt(u * u + v * v))
+        Qnew, F, G = generateQFG(r, u, v, E, p)
+        Fh, Gh = generateFhGh(Qnew, F, G, c, u, v)
+        Qnew = update(Qnew, dt, dx, dy, nx, ny, Fh, Gh)
+        Qnew[:, 0, :] = Qnew[:, 1, :]
+        Qnew[0, :, :] = Qnew[2, :, :]
+        Qnew[:, ny - 1, :] = Qnew[:, ny - 2, :]
+        Qnew[nx - 1, :, :] = Qnew[nx - 2, :, :]
+        # Primitive variables
+        r = Qnew[:, :, 0]
+        u = Qnew[:, :, 1] / r
+        v = Qnew[:, :, 2] / r
+        E = Qnew[:, :, 3]
+        p = (gamma - 1) * (E - 0.5 * r * (u * u + v * v))
+        c = np.sqrt(gamma * p / r)
+        t = t + dt  # Advance  time
+
+    return r, u, v,p, E,c,t
+
+@njit(parallel=True)
 def generateQFG(r, u, v, E, p):
     nx, ny = r.shape
     Q = np.zeros((nx, ny, 4))
     F = np.zeros((nx, ny, 4))
     G = np.zeros((nx, ny, 4))
-    for j in np.arange(0, ny):
-        for i in np.arange(0, nx):
+    for j in prange(0, ny):
+        for i in prange(0, nx):
             # Flux calculation
             Q[i, j, 0] = r[i, j]
             Q[i, j, 1] = r[i, j] * u[i, j]
@@ -33,7 +65,7 @@ def generateQFG(r, u, v, E, p):
             G[i, j, 3] = v[i, j] * (E[i, j] + p[i, j])
     return Q, F, G
 
-
+@njit(parallel=True)
 def generateFhGh(Q, F, G, c, u, v):
     nx, ny = c.shape
 
@@ -43,8 +75,8 @@ def generateFhGh(Q, F, G, c, u, v):
     Fh = np.zeros((nx - 1, ny, 4))
     Gh = np.zeros((nx, ny - 1, 4))
 
-    for j in range(0, ny - 1):
-        for i in range(0, nx - 1):
+    for j in prange(0, ny - 1):
+        for i in prange(0, nx - 1):
             ah1[i, j] = max(abs(u[i, j]) + c[i, j], abs(u[i + 1, j]) + c[i + 1, j])
             ah2[i, j] = max(abs(v[i, j]) + c[i, j], abs(v[i, j + 1]) + c[i, j + 1])
 
@@ -101,4 +133,14 @@ def RiamanProblemInit(nx=200,
                 v[i, j] = 1.206
             E[i, j] = p[i, j] / (gamma - 1) + 0.5 * r[i, j] * (u[i, j] ** 2 + v[i, j] ** 2)
             c[i, j] = np.sqrt(gamma * p[i, j] / r[i, j])
-    return r,u,v,p,E,c
+    return r, u, v, p, E, c, dx, dy
+
+if __name__=="__main__":
+    nx=15000
+    ny=15000
+    r0, u0, v0, p0, E0, c0, dx, dy = RiamanProblemInit(nx, ny)
+    results=simulation(r0, u0, v0, E0, p0,c0,
+               dx, dy, ny, nx, .3)
+    plt.imshow(results[0])
+    plt.show()
+    print("DONE")
